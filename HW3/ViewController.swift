1c5abb4034727fc1c920be2b70a604b8326aa81a
art1work <avermokhin@gmail.com>
2022-06-25 17:47:46 +0400

    Initial Commit

---
+++ HW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3
+//
+//  Created by Артём Ермохин on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
